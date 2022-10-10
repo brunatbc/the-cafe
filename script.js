34d922ba9cdccd241f17a7aa3fe5f76bcfb9b0ba
Bruna Teles <43624494+brunatbc@users.noreply.github.com>
2022-10-10 17:51:07 -0300

    refatorando e criando responsividade

---
+++ script.js
@@ -0,0 +1,11 @@
+function showMenu (){
+    let menuMobile = document.querySelector(".mobile-menu")
+
+    if (menuMobile.classList.contains("open"))
+
+        menuMobile.classList.remove("open");
+
+        else{
+            menuMobile.classList.add("open");
+        }
+}
